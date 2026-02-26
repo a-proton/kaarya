8e9841905bd41359f3fd317397b8ff0d88272556
Avinash Gautam <avinasgautam344@gmail.com>
2026-02-26 21:04:21 +0545

    build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://206.189.147.215/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
